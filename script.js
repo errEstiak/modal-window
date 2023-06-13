9189e4c6f71042fc5694396d3352b6213ed92095
errEstiak <estiak35-2998@diu.edu.bd>
2023-06-13 23:40:53 +0600

    selecting class through query selector & testing is it working

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
+//by selecting a class with multiple uses it will only effect on the first class 
+// we need the querySelectorAll for this
 
+const openBtn = document.querySelectorAll('.show-modal');
+const closeBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+// console.log(openBtn);
+
+for(let i=0; i < openBtn.length; i++){
+  // console.log(openBtn[i]);
+  console.log(openBtn[i].textContent);
+}
